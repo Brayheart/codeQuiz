09ed79fc79b4018214e381dfdd0061401d1e9145
Tyler Bray <broncobray77@gmail.com>
2021-07-13 14:30:38 -0700

    working on timer

--- index.js
+++ index.js
@@ -34,10 +34,41 @@ var questions = [
   }
 ];
 
-currentQuestion = 0
-userScore = 0
-startBtn = document.querySelector('#start')
-container = document.querySelector(".center")
+var currentQuestion = 0
+var userScore = 0
+var time = 5
+var startBtn = document.querySelector('#start')
+var container = document.querySelector(".center")
+var users = JSON.parse(localStorage.getItem('users')) || []
+
+
+function timer(){
+  setInterval(() => {    
+    if(time === 0){
+      container.textContent = ''
+      var text = document.createElement('h2')
+      text.textContent = 'GAME OVER'
+
+      var restart = document.createElement('button')
+      restart.textContent = 'restart'
+      restart.setAttribute('class', 'restart')
+
+
+      container.appendChild(text)
+      container.appendChild(restart)
+      document.querySelector('#time').textContent = `TIMES UP`
+      clearInterval(timer())
+    } else {
+
+      document.querySelector('#time').textContent = `Time: ${time}`
+      console.log(time)
+      time--
+    }
+  }, 1000);
+}
+
+
+console.log(typeof users, users)
 
 function startQuiz(){
   //clear container
@@ -46,24 +77,24 @@ function startQuiz(){
   // check to see if at end of quiz
   console.log(currentQuestion)
   if(currentQuestion === 4){
-    console.log("here")
+    highscore()
+  } else {
+    //create title
+    var title = document.createElement('h2')
+    title.textContent = questions[currentQuestion].title
+    title.setAttribute('class', 'title')
+    document.querySelector('.center').appendChild(title)
+  
+    //create buttons
+    questions[currentQuestion].choices.forEach(el => {
+      var button = document.createElement("button")
+      button.setAttribute('class', 'answer')
+      button.textContent = el
+      button.addEventListener("click", checkAnswer)
+      container.appendChild(button)
+    })
   }
 
-
-  //create title
-  var title = document.createElement('h2')
-  title.textContent = questions[currentQuestion].title
-  title.setAttribute('class', 'title')
-  document.querySelector('.center').appendChild(title)
-
-  //create buttons
-  questions[currentQuestion].choices.forEach(el => {
-    var button = document.createElement("button")
-    button.setAttribute('class', 'answer')
-    button.textContent = el
-    button.addEventListener("click", checkAnswer)
-    container.appendChild(button)
-  })
 
 }
 
@@ -77,6 +108,7 @@ function checkAnswer(event){
     container.appendChild(userAnswer)
     userScore++
   } else {
+    time = time - 10
     userAnswer.textContent = 'Wrong!'
     userAnswer.setAttribute('class','wrong')
     container.appendChild(userAnswer)
@@ -88,5 +120,104 @@ function checkAnswer(event){
   startQuiz()
 }
 
+function highscore(){
+  container.innerHTML = ''
+  var title = document.createElement("h3")
+  title.textContent = 'All Done!'
+
+  var finalScore = document.createElement('h2')
+  finalScore.textContent = `Your final score: ${userScore}`
+
+  var userInput = document.createElement('h2')
+  userInput.setAttribute('class', 'userTitle')
+  userInput.textContent = 'Enter Your Initials: '
+
+  var inputContainer = document.createElement('div')
+  var input = document.createElement('input')
+  input.setAttribute('id', 'textarea')
+  inputContainer.appendChild(input)
+  var button = document.createElement("Button")
+  button.textContent = "Submit"
+  button.setAttribute('id','input')
+  button.addEventListener('click', setHighScore)
+  inputContainer.setAttribute('class','input')
+  inputContainer.appendChild(button)
+  
+  container.appendChild(title)
+  container.appendChild(finalScore)
+  container.appendChild(userInput)
+  container.appendChild(inputContainer)
+}
+
+function setHighScore(event){
+  userInitals = document.querySelector('#textarea').value
+  users.push([userInitals, userScore])
+  localStorage.setItem('users', JSON.stringify(users))
+  viewHighScore()
+}
+
+function viewHighScore(){
+  container.innerHTML = ''
+
+  var title = document.createElement('h2')
+  title.textContent = 'High Scores'
+  container.appendChild(title)
+
+  var table  = document.createElement("table")
+  var tablerow = document.createElement('tr')
+  var usertableheader = document.createElement('th')
+  usertableheader.textContent = 'user'
+  var scoretableheader = document.createElement('th')
+  scoretableheader.textContent = 'score'
+
+  tablerow.appendChild(usertableheader)
+  tablerow.appendChild(scoretableheader)
+  table.appendChild(tablerow)
+  container.appendChild(table)
+
+  users.forEach(el => {
+    var user = el[0]
+    var score = el[1]
+    var tablerow = document.createElement('tr')
+    var tabledatauser = document.createElement('td')
+    var tabledatascore = document.createElement('td')
+
+    tabledatauser.textContent = user
+    tabledatascore.textContent = score
+    
+    tablerow.appendChild(tabledatauser)
+    tablerow.appendChild(tabledatascore)
+    table.appendChild(tablerow)
+  })
+
+  var buttoncontainer = document.createElement('div')
+  buttoncontainer.setAttribute('class','buttoncontainer')
+  var restart = document.createElement('button')
+  restart.textContent = 'restart'
+  restart.setAttribute('class', 'restart')
+  var clear = document.createElement('button')
+  clear.textContent = 'clear highscores'
+  clear.setAttribute('class','highscoretitle')
+
+  buttoncontainer.appendChild(restart)
+  buttoncontainer.appendChild(clear)
+
+  container.appendChild(buttoncontainer)
+
+  document.querySelector('.highscoretitle').addEventListener('click',clearHighScores)
+  document.querySelector('.restart').addEventListener('click',reload)
+}
+
+function clearHighScores(){
+  users = []
+  localStorage.clear()
+  viewHighScore()
+}
+
+function reload(){
+  location.reload();
+}
+
 
 startBtn.addEventListener("click", startQuiz);
+startBtn.addEventListener("click", timer);
